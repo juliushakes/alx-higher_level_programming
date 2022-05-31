a8fcd7be501e01f23bfe735c7b34210c7464cea7
juliushakes <juliustusing98@gmail.com>
2022-05-31 02:19:53 -0700

    Comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         print(f
-            "{i}{j}"),
+            "{i:d}{j:d}"),
             end=", " if int(str(i) + str(j)) < 89 else "\n"
             )
